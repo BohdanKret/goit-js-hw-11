11b6dc0d37638aa815a438b0f5c28ac9a232a141
Bohdan Kret <kretbohdan2002@gmail.com>
2024-11-17 17:05:07 +0200

    homework update #2

--- src/main.js
+++ src/main.js
@@ -3,32 +3,23 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 // Описаний у документації
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import dataRequest from './js/pixabay-api';
 import makeMarkup from './js/render-functions';
 
 const form = document.querySelector('.form');
+const loader = document.querySelector('#loader');
 const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', onBtnSubmit);
 
-let onPictureClick = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
-
-onPictureClick.on('show.simplelightbox', function () {
-  onPictureClick.refresh();
-});
-
-
 function onBtnSubmit(event) {
   event.preventDefault();
+    gallery.innerHTML = '';
+  loader.classList.remove('visually-hidden');
 
   const userRequest = event.currentTarget.elements.search.value.trim();
 
@@ -38,6 +29,8 @@ function onBtnSubmit(event) {
 
   dataRequest(userRequest)
     .then(data => {
+      loader.classList.add('visually-hidden');
+
       if (data.hits.length === 0) {
         iziToast.error({
           message:
@@ -52,6 +45,15 @@ function onBtnSubmit(event) {
       }
       // console.log('then', makeMarkup(data.hits));
       gallery.innerHTML = makeMarkup(data.hits);
+
+      let onPictureClick = new SimpleLightbox('.imgblock a', {
+        captions: true,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+      });
+
+      onPictureClick.refresh();
     })
     .catch(error => console.log(error));
 
